f8bc60d47950d25307cc92f3055296191d43a568
Andri Sunardi <account@andrisunardi.com>
2026-01-09 17:14:25 +0800

    Create Console Commands Database Backup Command

---
+++ app/Console/Commands/DatabaseBackupCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class DatabaseBackupCommand extends Command
+{
+    protected $signature = 'database:backup';
+
+    protected $description = 'Database Backup';
+
+    public function handle(): bool
+    {
+        $username = config('database.mysql.username');
+        $password = config('database.mysql.password');
+        $database = config('database.mysql.database');
+
+        $date = today()->toDateString();
+        $path = storage_path("app/private/database/{$date}.sql");
+
+        exec("mysqldump -u {$username} -p{$password} {$database} > {$path}");
+
+        $this->info('Database Backup is Completed.');
+        Log::info('Database Backup is Completed.');
+
+        return Command::SUCCESS;
+    }
+}
