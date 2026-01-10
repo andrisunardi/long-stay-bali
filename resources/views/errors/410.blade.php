e9d26f217ee255a6d11ab3c5f1f47da6c21dc12d
Andri Sunardi <account@andrisunardi.com>
2026-01-10 16:42:43 +0800

    Create All Errors

---
+++ resources/views/errors/410.blade.php
@@ -0,0 +1,6 @@
+@extends('components.layouts.app')
+
+@section("title", "410 - Gone")
+@section("code", "410")
+@section("message", "Gone")
+@section("description", "The requested page is no longer available")
