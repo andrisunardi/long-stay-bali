951d2c08d495b43c72eb207bb488085351cf60ac
Andri Sunardi <account@andrisunardi.com>
2026-05-04 03:23:20 +0700

    Update Property Detail Page

--- resources/views/pages/property/detail.blade.php
+++ resources/views/pages/property/detail.blade.php
@@ -27,22 +27,38 @@ new #[Title('Property Detail')] class extends Component {
                     <div class="ratio ratio-16x9 h-100">
                         <img draggable="false"
                             class="img-fluid w-100 h-100 rounded object-fit-cover user-select-none pe-none"
-                            src="{{ $property->image?->image_url ?? asset('images/image-not-available.png') }}"
+                            src="{{ $property->image?->image_url ?? asset('images/placeholder.png') }}"
                             alt="{{ trans('property.property') }} - {{ $property->name }} - {{ config('constants.title') }}" />
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="row g-3">
-                        @foreach ($property->images->take(4) as $propertyImage)
+                        @php
+                            $propertyImages = $property->images->take(4);
+                            $imageCount = $propertyImages->count();
+                        @endphp
+
+                        @foreach ($propertyImages as $propertyImage)
                             <div class="col-6" wire:key="property-image-{{ $propertyImage->id }}">
                                 <div class="ratio ratio-16x9">
                                     <img draggable="false"
                                         class="img-fluid w-100 h-100 rounded object-fit-cover user-select-none pe-none"
-                                        src="{{ $propertyImage->image_url ?? asset('images/image-not-available.png') }}"
+                                        src="{{ $propertyImage->image_url ?? asset('images/placeholder.png') }}"
                                         alt="{{ trans('property.property') }} - {{ trans('property.image') }} - {{ $property->name }} - {{ config('constants.title') }}">
                                 </div>
                             </div>
                         @endforeach
+
+                        @for ($i = $imageCount; $i < 4; $i++)
+                            <div class="col-6" wire:key="property-placeholder-{{ $i }}">
+                                <div class="ratio ratio-16x9">
+                                    <img draggable="false"
+                                        class="img-fluid w-100 h-100 rounded object-fit-cover user-select-none pe-none"
+                                        src="{{ asset('images/placeholder.png') }}"
+                                        alt="{{ trans('property.property') }} - {{ trans('property.image') }} - {{ $property->name }} - {{ config('constants.title') }}">
+                                </div>
+                            </div>
+                        @endfor
                     </div>
                 </div>
             </div>
