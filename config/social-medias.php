d6e85b401e6a65aab877bbfcda83cc4657b3e957
Andri Sunardi <account@andrisunardi.com>
2026-05-05 08:31:26 +0700

    Update Config Social Media Hide Tiktok And Linkedin

--- config/social-medias.php
+++ config/social-medias.php
@@ -17,18 +17,18 @@ return [
         'username' => config('constants.social_media.instagram'),
         'link' => 'https://www.instagram.com/'.config('constants.social_media.instagram'),
     ],
-    [
-        'id' => $id,
-        'name' => 'Tiktok',
-        'icon' => 'fab fa-tiktok',
-        'username' => config('constants.social_media.tiktok'),
-        'link' => 'https://www.tiktok.com/@'.config('constants.social_media.tiktok'),
-    ],
-    [
-        'id' => $id,
-        'name' => 'Linkedin',
-        'icon' => 'fab fa-linkedin',
-        'username' => config('constants.social_media.linkedin'),
-        'link' => 'https://www.linkedin.com/company/'.config('constants.social_media.linkedin'),
-    ],
+    // [
+    //     'id' => $id,
+    //     'name' => 'Tiktok',
+    //     'icon' => 'fab fa-tiktok',
+    //     'username' => config('constants.social_media.tiktok'),
+    //     'link' => 'https://www.tiktok.com/@'.config('constants.social_media.tiktok'),
+    // ],
+    // [
+    //     'id' => $id,
+    //     'name' => 'Linkedin',
+    //     'icon' => 'fab fa-linkedin',
+    //     'username' => config('constants.social_media.linkedin'),
+    //     'link' => 'https://www.linkedin.com/company/'.config('constants.social_media.linkedin'),
+    // ],
 ];
