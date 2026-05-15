12f01413b7b60823cb5a944582689d72517bfb62
Andri Sunardi <account@andrisunardi.com>
2026-05-15 14:23:03 +0700

    Create Libraries Google Translate

---
+++ app/Libraries/GoogleTranslate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Stichoza\GoogleTranslate\GoogleTranslate as Translator;
+
+class GoogleTranslate
+{
+    protected array $locales = [
+        'id',
+        'zh',
+        'fr',
+    ];
+
+    public function translateModel(
+        Model $model,
+        bool $force = false,
+    ): void {
+        if (! property_exists($model, 'translatable')) {
+            return;
+        }
+
+        $updates = [];
+
+        foreach ($model->translatable as $field) {
+            $value = $model->{$field};
+
+            if (blank($value)) {
+                continue;
+            }
+
+            if (
+                ! $force &&
+                method_exists($model, 'wasChanged') &&
+                ! $model->wasChanged($field) &&
+                ! $model->wasRecentlyCreated
+            ) {
+                continue;
+            }
+
+            foreach ($this->locales as $locale) {
+                $column = "{$field}_{$locale}";
+
+                $updates[$column] = (new Translator($locale))
+                    ->translate($value);
+            }
+        }
+
+        if (! empty($updates)) {
+            $model->update($updates);
+        }
+    }
+}
