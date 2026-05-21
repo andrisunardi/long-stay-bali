34056952a24d855762932b6eb41461d26d85ff66
Andri Sunardi <account@andrisunardi.com>
2026-05-22 02:57:33 +0700

    Update GoHighLevel To Service

--- app/Http/Controllers/GoHighLevelController.php
+++ app/Http/Controllers/GoHighLevelController.php
@@ -2,38 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Oauth;
+use App\Libraries\GoHighLevel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class GoHighLevelController extends Controller
 {
     public function oauth(Request $request)
     {
-        $code = $request->query('code');
-
-        $response = Http::asForm()->post(config('constants.ghl.oauth_url'), [
-            'client_id' => config('constants.ghl.client_id'),
-            'client_secret' => config('constants.ghl.client_secret'),
-            'grant_type' => 'authorization_code',
-            'code' => $code,
-            'redirect_uri' => config('constants.ghl.redirect_uri'),
-            'user_type' => config('constants.ghl.user_type'),
-        ]);
-
-        $data = $response->json();
-
-        if (isset($data['error'])) {
-            abort(400);
-        }
-
-        $oauth = Oauth::firstOrCreate(['code' => 'GOHIGHLEVEL']);
-        $oauth->refresh_token = $data['refresh_token'];
-        $oauth->access_token = $data['access_token'];
-        $oauth->token_type = $data['token_type'];
-        $oauth->expires_in = $data['expires_in'];
-        $oauth->scope = $data['scope'];
-        $oauth->save();
+        (new GoHighLevel())->oauth(code: $request->query('code'));
 
         session()->flash('success', [
             'title' => trans('index.connect').' '.trans('index.success'),
@@ -45,30 +21,7 @@ class GoHighLevelController extends Controller
 
     public function refresh()
     {
-        $oauth = Oauth::firstOrCreate(['code' => 'GOHIGHLEVEL']);
-
-        $response = Http::asForm()->post(config('constants.ghl.oauth_url'), [
-            'client_id' => config('constants.ghl.client_id'),
-            'client_secret' => config('constants.ghl.client_secret'),
-            'grant_type' => 'refresh_token',
-            'refresh_token' => $oauth->refresh_token,
-            'user_type' => config('constants.ghl.user_type'),
-            'redirect_uri' => config('constants.ghl.redirect_uri'),
-        ]);
-
-        $data = $response->json();
-
-        if (isset($data['error'])) {
-            abort(400);
-        }
-
-        $oauth = Oauth::where('code', 'GOHIGHLEVEL')->firstOrFail();
-        $oauth->refresh_token = $data['refresh_token'];
-        $oauth->access_token = $data['access_token'];
-        $oauth->token_type = $data['token_type'];
-        $oauth->expires_in = $data['expires_in'];
-        $oauth->scope = $data['scope'];
-        $oauth->save();
+        (new GoHighLevel())->refresh();
 
         session()->flash('success', [
             'title' => trans('index.refresh').' '.trans('index.success'),
--- app/Libraries/GoHighLevel.php
+++ app/Libraries/GoHighLevel.php
@@ -106,4 +106,62 @@ class GoHighLevel
             throw $e;
         }
     }
+
+    public function oauth(string $code): Oauth
+    {
+        $response = Http::asForm()->post(config('constants.ghl.oauth_url'), [
+            'client_id' => config('constants.ghl.client_id'),
+            'client_secret' => config('constants.ghl.client_secret'),
+            'grant_type' => 'authorization_code',
+            'code' => $code,
+            'redirect_uri' => config('constants.ghl.redirect_uri'),
+            'user_type' => config('constants.ghl.user_type'),
+        ]);
+
+        $data = $response->json();
+
+        if (isset($data['error'])) {
+            abort(400);
+        }
+
+        $oauth = Oauth::firstOrCreate(['code' => 'GOHIGHLEVEL']);
+        $oauth->refresh_token = $data['refresh_token'];
+        $oauth->access_token = $data['access_token'];
+        $oauth->token_type = $data['token_type'];
+        $oauth->expires_in = $data['expires_in'];
+        $oauth->scope = $data['scope'];
+        $oauth->save();
+
+        return $oauth;
+    }
+
+    public function refresh( ): Oauth
+    {
+        $oauth = Oauth::firstOrCreate(['code' => 'GOHIGHLEVEL']);
+
+        $response = Http::asForm()->post(config('constants.ghl.oauth_url'), [
+            'client_id' => config('constants.ghl.client_id'),
+            'client_secret' => config('constants.ghl.client_secret'),
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $oauth->refresh_token,
+            'user_type' => config('constants.ghl.user_type'),
+            'redirect_uri' => config('constants.ghl.redirect_uri'),
+        ]);
+
+        $data = $response->json();
+
+        if (isset($data['error'])) {
+            abort(400);
+        }
+
+        $oauth = Oauth::where('code', 'GOHIGHLEVEL')->firstOrFail();
+        $oauth->refresh_token = $data['refresh_token'];
+        $oauth->access_token = $data['access_token'];
+        $oauth->token_type = $data['token_type'];
+        $oauth->expires_in = $data['expires_in'];
+        $oauth->scope = $data['scope'];
+        $oauth->save();
+
+        return $oauth;
+    }
 }
