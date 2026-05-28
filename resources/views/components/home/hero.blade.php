40771d782c5db5aefc0500fbf94ccfff5543f1cf
Andri Sunardi <account@andrisunardi.com>
2026-05-28 20:15:36 +0700

    Update Home Hero

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -4,27 +4,24 @@
     'image' => null,
 ])
 
-<div class="vh-100 position-relative">
-    <div class="d-none d-sm-inline d-md-none d-lg-inline">
-        <img draggable="false" loading="lazy" decoding="async" class="w-100 h-100 object-fit-cover user-select-none pe-none"
-            src="{{ $image }}"
-            alt="{{ trans('index.banner') }} - {{ trans('page.home') }} - {{ config('constants.name') }}">
-    </div>
-
-    <div class="d-inline d-sm-none d-md-inline d-lg-none">
-        <img draggable="false" loading="lazy" decoding="async"
-            class="w-100 h-100 object-fit-cover user-select-none pe-none" src="{{ $image }}"
-            alt="{{ trans('index.banner') }} - {{ trans('page.home') }} - {{ config('constants.name') }}">
-    </div>
+<div class="position-relative">
+    <img draggable="false" loading="lazy" decoding="async"
+        class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover user-select-none pe-none"
+        src="{{ $image }}"
+        alt="{{ trans('index.banner') }} - {{ trans('page.home') }} - {{ config('constants.name') }}">
 
     <div class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
 
-    <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center">
-        <div class="container-md">
-            <div class="row g-4">
-                <div class="col-lg-6">
-                    <div class="lead text-white">{{ $title }}</div>
-                    <div class="display-6 fw-bold text-white">{!! $description !!}</div>
+    <div class="position-relative">
+        <div class="container-md pt-5">
+            <div class="row align-items-center min-vh-100 g-4 py-5">
+                <div class="col-lg-6 text-white">
+                    <div class="lead mb-3">
+                        {{ $title }}
+                    </div>
+                    <div class="display-5 fw-bold">
+                        {!! $description !!}
+                    </div>
                 </div>
                 <div class="col-lg-6 col-xl-5 offset-xl-1">
                     <livewire:home.search />
