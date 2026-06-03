cc255438170d9d62d228a2137dab80feafae3bb4
Andri Sunardi <account@andrisunardi.com>
2026-06-04 01:34:49 +0700

    Update Form Tinymce

--- resources/views/components/form/tinymce.blade.php
+++ resources/views/components/form/tinymce.blade.php
@@ -8,10 +8,16 @@
 </div>
 
 <script>
-    document.addEventListener('livewire:init', () => {
+    function initTinyMce() {
+        const existing = tinymce.get('{{ $id }}');
+
+        if (existing) {
+            existing.remove();
+        }
+
         tinymce.init({
             selector: '#{{ $id }}',
-            height: 500,
+            height: 300,
             plugins: [
                 'code',
                 'table',
@@ -22,11 +28,25 @@
                 'fullscreen'
             ],
             toolbar: 'undo redo | blocks | styles | bold italic underline | indent outdent | alignleft aligncenter alignright | bullist numlist | link image table | | code fullscreen',
+            images_file_types: 'png,jpg,jpeg,gif,svg,webp',
+            file_picker_types: 'file image media',
+            images_upload_url: '/cms/upload/image',
+            images_upload_credentials: true,
+            block_unsupported_drop: true,
+            automatic_uploads: true,
             setup: function(editor) {
                 editor.on('change keyup', function() {
                     @this.set('form.{{ $id }}', editor.getContent());
                 });
             }
+        });
+    }
+
+    initTinyMce();
+
+    document.addEventListener('livewire:navigated', () => {
+        queueMicrotask(() => {
+            initTinyMce();
         });
     });
 </script>
