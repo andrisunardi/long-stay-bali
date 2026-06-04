6fd9180546a2c213e2271c65de13505ccf01eed6
Andri Sunardi <account@andrisunardi.com>
2026-06-04 16:55:54 +0700

    Update Search Price

--- resources/views/components/search/price.blade.php
+++ resources/views/components/search/price.blade.php
@@ -40,9 +40,9 @@
                         {{ trans('home.search.from') }}
                     </label>
 
-                    <input type="number" class="form-control form-control-sm" id="price_min" name="price_min"
-                        min="{{ $priceMin }}" max="{{ $priceMax }}" value="{{ $prices['min'] ?? 0 }}"
-                        step="1000000" wire:model.live.debounce.5s="prices.min">
+                    <input type="text" class="form-control form-control-sm autonumeric" id="price_min"
+                        name="price_min" min="{{ $priceMin }}" max="{{ $priceMax }}"
+                        value="{{ $prices['min'] ?? 0 }}" step="1000000">
                 </div>
 
                 <div class="col-6">
@@ -50,9 +50,9 @@
                         {{ trans('home.search.to') }}
                     </label>
 
-                    <input type="number" class="form-control form-control-sm" id="price_max" name="price_max"
-                        min="{{ $priceMin }}" max="{{ $priceMax }}" value="{{ $prices['max'] ?? 0 }}"
-                        step="1000000" wire:model.live.debounce.5s="prices.max">
+                    <input type="text" class="form-control form-control-sm autonumeric" id="price_max"
+                        name="price_max" min="{{ $priceMin }}" max="{{ $priceMax }}"
+                        value="{{ $prices['max'] ?? 0 }}" step="1000000">
                 </div>
             </div>
         </div>
@@ -61,6 +61,45 @@
 
 @script
     <script>
+        function autoNumericInit() {
+            const $min = $("#price_min");
+            const $max = $("#price_max");
+
+            if ($min.length) {
+                if ($min[0].autoNumeric) {
+                    $min.autoNumeric("destroy");
+                }
+
+                $min.autoNumeric("init", {
+                    aSep: ".",
+                    aDec: ",",
+                    mDec: "0"
+                });
+
+                $min.off("input change").on("input change", function() {
+                    const raw = $(this).val().replace(/\./g, '');
+                    @this.set('prices.min', parseInt(raw || 0));
+                });
+            }
+
+            if ($max.length) {
+                if ($max[0].autoNumeric) {
+                    $max.autoNumeric("destroy");
+                }
+
+                $max.autoNumeric("init", {
+                    aSep: ".",
+                    aDec: ",",
+                    mDec: "0"
+                });
+
+                $max.off("input change").on("input change", function() {
+                    const raw = $(this).val().replace(/\./g, '');
+                    @this.set('prices.max', parseInt(raw || 0));
+                });
+            }
+        }
+
         function priceSlider() {
             const priceSlider = document.getElementById('price-slider');
 
@@ -84,22 +123,38 @@
                 });
 
                 priceSlider.noUiSlider.on('change', (values) => {
-                    $wire.set('prices.min', parseInt(values[0]));
-                    $wire.set('prices.max', parseInt(values[1]));
+                    const min = parseInt(values[0]);
+                    const max = parseInt(values[1]);
+
+                    $wire.set('prices.min', min);
+                    $wire.set('prices.max', max);
+
+                    const priceMin = $("#price_min");
+                    if (priceMin.length && priceMin.data('autoNumeric')) {
+                        priceMin.autoNumeric("set", min);
+                    }
+
+                    const priceMax = $("#price_max");
+                    if (priceMax.length && priceMax.data('autoNumeric')) {
+                        priceMax.autoNumeric("set", max);
+                    }
                 });
             }
         }
 
+        autoNumericInit();
         priceSlider();
 
         document.addEventListener('livewire:navigated', () => {
             queueMicrotask(() => {
+                autoNumericInit();
                 priceSlider();
             });
         });
 
         window.addEventListener('price-slider', () => {
             queueMicrotask(() => {
+                autoNumericInit();
                 priceSlider();
             });
         });
