03f1fbe4e4da6475a9077850d3467aae2d1b0e2f
Andri Sunardi <account@andrisunardi.com>
2026-06-04 11:34:09 +0700

    Update Livewire Config Add Payload Max Size

--- config/livewire.php
+++ config/livewire.php
@@ -68,7 +68,7 @@ return [
     'csp_safe' => false,
 
     'payload' => [
-        'max_size' => 1024 * 1024,
+        'max_size' => 10 * 1024 * 1024,
         'max_nesting_depth' => 10,
         'max_calls' => 50,
         'max_components' => 20,
