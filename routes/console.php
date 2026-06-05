05be2a49181052f52169ce61d34fbf30a193a2a9
Andri Sunardi <account@andrisunardi.com>
2026-06-05 16:27:09 +0700

    Update Routes Console

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,23 @@
 <?php
 
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Artisan::command('logs:clear', function () {
+    $files = File::glob(storage_path('logs/*.log'));
+
+    foreach ($files as $file) {
+        File::delete($file);
+        $this->line(basename($file));
+    }
+
+    $this->info('All Laravel logs have been deleted.');
+})->purpose('Delete all Laravel log files');
 
 Schedule::command('ghl:refresh-token')->daily();
