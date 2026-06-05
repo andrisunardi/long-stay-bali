4b068ed421348d499c9dc2b7bf70719a245acdac
Andri Sunardi <account@andrisunardi.com>
2026-06-05 16:03:47 +0700

    Update Alert Error And Property Edit

--- resources/views/components/alert-error.blade.php
+++ resources/views/components/alert-error.blade.php
@@ -1,12 +1,11 @@
 <div>
     @if ($errors?->any() ?? null)
-        <div class="alert alert-danger">
+        <div class="alert alert-danger text-capitalize">
             @foreach ($errors->all() as $message)
-                {{ $message }}
+                {{ is_array($message) ? implode(' ', $message) : $message }}<br>
                 @if (!$loop->last)
                     <br />
                 @endif
             @endforeach
         </div>
     @endif
-</div>
