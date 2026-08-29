83b0f0cb914a369c704b4be5217440f1897b07cb
Andri Sunardi <account@andrisunardi.com>
2026-08-29 17:49:11 +0700

    Update CMS Property Detail Page

--- resources/views/pages/cms/property/⚡detail.blade.php
+++ resources/views/pages/cms/property/⚡detail.blade.php
@@ -871,6 +871,45 @@ new #[Title('Detail | Property')] class extends Component {
                 <br />
 
                 <h5 class="fw-bold text-uppercase border-bottom pb-3">
+                    {{ trans('property.listing_information') }}
+                </h5>
+
+                <div class="row">
+                    <div class="col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                        <div class="fw-bold">{{ trans('property.listing_type') }}</div>
+                    </div>
+                    <div class="col-sm-7 col-md-8 col-lg-9 col-xl-10">
+                        {{ $property->listing_type ? Str::headline($property->listing_type->name) : '-' }}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                        <div class="fw-bold">{{ trans('property.reference') }}</div>
+                    </div>
+                    <div class="col-sm-7 col-md-8 col-lg-9 col-xl-10">
+                        {{ $property->reference ?? '-' }}
+                    </div>
+                </div>
+
+                <br />
+
+                <h5 class="fw-bold text-uppercase border-bottom pb-3">
+                    {{ trans('property.sale_information') }}
+                </h5>
+
+                <div class="row">
+                    <div class="col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                        <div class="fw-bold">{{ trans('property.currency') }}</div>
+                    </div>
+                    <div class="col-sm-7 col-md-8 col-lg-9 col-xl-10">
+                        {{ $property->currency?->name ?? '-' }}
+                    </div>
+                </div>
+
+                <br />
+
+                <h5 class="fw-bold text-uppercase border-bottom pb-3">
                     {{ trans('property.images') }}
                 </h5>
 
